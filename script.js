7962c31f4717b94faefad70e469c67f169d8489f
Ganpat kakar <kakarganpat@gmail.com>
2016-07-11 16:16:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,358 @@
+$(document).ready(function() {
+
+
+$(".slideInDown").click(function(){
+  $(".appguru").addClass("slidindown");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+$(".slideInLeft").click(function(){
+  $(".appguru").addClass("slidinleft");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+$(".slideInRight").click(function(){
+  $(".appguru").addClass("slideinright");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+$(".slideInUp").click(function() {
+  $(".appguru").addClass("slideinup");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+$(".slideOutDown").click(function(){
+  $(".appguru").addClass("slideoutdown");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+$(".slideOutLeft").click(function(){
+  $(".appguru").addClass("slideoutleft");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+$(".slideOutRight").click(function(){
+  $(".appguru").addClass("slideoutright");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+$(".slideOutUp").click(function(){
+  $(".appguru").addClass("slideoutup");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+
+$(".fadeIN").click(function(){
+  $(".appguru").addClass("fadein");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+$(".fadeDown").click(function(){
+  $(".appguru").addClass("fadedown");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+$(".fadeDownBig").click(function(){
+  $(".appguru").addClass("fadedownbig");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+$(".fadeInLeft").click(function(){
+  $(".appguru").addClass("fadeinleft");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+$(".fadeInLeftBig").click(function(){
+  $(".appguru").addClass("fadeinleftbig");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+$(".fadeInRight").click(function(){
+  $(".appguru").addClass("Fadeinright");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+$(".fadeInRightBig").click(function(){
+  $(".appguru").addClass("Fadeinrightbig");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+$(".fadeInUp").click(function(){
+  $(".appguru").addClass("Fadeinup");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+$(".fadeOut").click(function(){
+  $(".appguru").addClass("Fadeout");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+$(".fadeOutDown").click(function(){
+  $(".appguru").addClass("Fadeoutdown");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+$(".fadeOutDownBig").click(function(){
+  $(".appguru").addClass("Fadeoutdownbig");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+$(".fadeOutLeft").click(function(){
+  $(".appguru").addClass("Fadeoutleft");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+$(".fadeOutLeftBig").click(function(){
+  $(".appguru").addClass("Fadeoutleftbig");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+$(".fadeOutRight").click(function(){
+  $(".appguru").addClass("Fadeoutright");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+$(".fadeOutRightBig").click(function(){
+  $(".appguru").addClass("Fadeoutrightbig");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+$(".fadeOutUp").click(function(){
+  $(".appguru").addClass("Fadeoutup");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+$(".fadeOutUpBig").click(function(){
+  $(".appguru").addClass("Fadeoutupbig");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+
+/*Zoom Effect animation*/
+
+$(".zoomIn").click(function(){
+  $(".appguru").addClass("zoomin");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+$(".zoomInDown").click(function(){
+  $(".appguru").addClass("zoomindown");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+$(".zoomInLeft").click(function(){
+  $(".appguru").addClass("zoominleft");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+$(".zoomInRight").click(function(){
+  $(".appguru").addClass("zoominright");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+$(".zoomInUp").click(function(){
+  $(".appguru").addClass("zoominup");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+$(".zoomOut").click(function(){
+  $(".appguru").addClass("zoomout");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+$(".zoomOutDown").click(function(){
+  $(".appguru").addClass("zoomoutdown");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+$(".zoomOutLeft").click(function(){
+  $(".appguru").addClass("zoomoutleft");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1500);
+});
+
+$(".zoomOutRight").click(function(){
+  $(".appguru").addClass("zoomoutright");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+$(".zoomOutUp").click(function(){
+  $(".appguru").addClass("zoomoutup");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+$(".bounce").click(function(){
+  $(".appguru").addClass("bounce1");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+$(".bounceIn").click(function(){
+  $(".appguru").addClass("bouncein");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+$(".bounceInDown").click(function(){
+  $(".appguru").addClass("bounceindown");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+$(".bounceInLeft").click(function(){
+  $(".appguru").addClass("bounceinleft");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+$(".bounceInRight").click(function(){
+  $(".appguru").addClass("bounceinright");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+$(".bounceInUp").click(function(){
+  $(".appguru").addClass("bounceinup");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+$(".bounceOut").click(function(){
+  $(".appguru").addClass("bounceout");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+$(".bounceOutDown").click(function(){
+  $(".appguru").addClass("bounceoutdown");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+$(".bounceOutLeft").click(function(){
+  $(".appguru").addClass("bounceoutleft");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+$(".bounceOutRight").click(function(){
+  $(".appguru").addClass("bounceoutright");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+$(".bounceOutUp").click(function(){
+  $(".appguru").addClass("bounceoutup");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+$(".rotateIn").click(function(){
+  $(".appguru").addClass("rotatein");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+$(".rotateInDownLeft").click(function(){
+  $(".appguru").addClass("rotateindownleft");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+$(".rotateInDownRight").click(function(){
+  $(".appguru").addClass("rotateindownright");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+$(".rotateInUpLeft").click(function(){
+  $(".appguru").addClass("rotateinupleft");
+  setTimeout(function () {
+    $(".appguru").removeClass().addClass("appguru");
+  }, 1000);
+});
+
+});
